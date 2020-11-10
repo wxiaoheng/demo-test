66bb9d893a354e18b36d017df2fa5b0943e552c2
wangxh <o_shorn@163.com>
2020-11-10 19:22:41 +0800

    test

--- demo-module/src/main/java/com/hundsun/jres/studio/demo/App.java
+++ demo-module/src/main/java/com/hundsun/jres/studio/demo/App.java
@@ -6,7 +6,6 @@ package com.hundsun.jres.studio.demo;
  * @since
  */
 public class App {
-
     public static void main( String[] args ) {
         //asd
         System.out.println( "Hello World!" );
